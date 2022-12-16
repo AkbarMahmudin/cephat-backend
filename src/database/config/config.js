a8e9fb7e4282468aa4b65acafc07e7ef82963a28
AkbarMahmudin <wastanami17@gmail.com>
2022-12-16 12:46:33 +0700

    update config db

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -14,20 +14,35 @@ module.exports = {
     password: DBPASSWORD,
     database: DBNAME,
     host: DBHOST,
-    dialect: DBDIALECT
+    dialect: DBDIALECT,
+    pool: {
+      max: 5,
+      min: 0,
+      idle: 1 // Keep this very low or it'll make all Lambda requests take longer
+    }
   },
   test: {
     username: DBUSERNAME,
     password: DBPASSWORD,
     database: DBNAME,
     host: DBHOST,
-    dialect: DBDIALECT
+    dialect: DBDIALECT,
+    pool: {
+      max: 5,
+      min: 0,
+      idle: 1 // Keep this very low or it'll make all Lambda requests take longer
+    }
   },
   production: {
     username: DBUSERNAME,
     password: DBPASSWORD,
     database: DBNAME,
     host: DBHOST,
-    dialect: DBDIALECT
+    dialect: DBDIALECT,
+    pool: {
+      max: 5,
+      min: 0,
+      idle: 1 // Keep this very low or it'll make all Lambda requests take longer
+    }
   }
 }
